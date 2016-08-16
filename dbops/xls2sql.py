f383bc1eea01d69bcb1d22a54915479677ff051e
Ryan Birmingham <birm@rbirm.us>
2016-08-16 13:21:36 -0400

    added 3 layer xls conv

--- dbops/xls2sql.py
+++ dbops/xls2sql.py
@@ -8,7 +8,7 @@ class Xls2Sql(object):
                  table="dual", fin="patch.xls",
                  findopt={"Id": 1, "Name": 2}, lookup="", skiprows=1,
                  range2=[2, 3], t1="mysql.user", t2="mysql.user",
-                 db="schemadb"):
+                 db="schemadb", t3="", d="", lookup2="", ina="", inb=""):
         """initalize the conversion."""
         self.xls = xlrd.open_workbook(fin).sheet_by_index(0)  # sheet
         # discard rows to skip in xls
@@ -17,12 +17,23 @@ class Xls2Sql(object):
         self.table = table
         self.t1 = t1
         self.t2 = t2
+        self.t3 = t3
         self.a = a
         self.b = b
         self.c = c
+        self.d = d
         self.findopt = findopt
         self.lookup = lookup
+        self.lookup2 = lookup2
         self.db = db
+        if not ina:
+            self.ina = self.a
+        else:
+            self.ina = ina
+        if not inb:
+            self.inb = self.b
+        else:
+            self.inb = inb
 
     def query_gen_2(self):
         """Create a query for 2 cols (a relation between existing entities)."""
@@ -53,6 +64,38 @@ class Xls2Sql(object):
                     query = query + " (" + alpha + "," + beta + "),"
         return query[:-1]  # remove last comma and return
 
+    def query_gen_3(self):
+        """Make relation between two tables using a third as lookup."""
+        query = "use " + self.db + "; \n"
+        prefix1 = "insert ignore into " + self.table
+        prefix1 = prefix1 + " (" + self.ina + "," + self.inb + ") select "
+        for rownum in range(self.skiprows, self.xls.nrows):
+            query = query + prefix1
+            row = self.xls.row_values(rownum)
+            alpha = "(select " + self.a + " from " + self.t1 + " where "
+            for key, value in self.findopt.iteritems():
+                alpha = alpha + key + " = "
+                alpha = alpha + "'" + row[value].replace("'", r"\'")
+                alpha = alpha + "'" + " and "
+            alpha = alpha[:-4] + " limit 1), "  # remove last 'and'
+            alpha = alpha + " " + self.d + " from " + self.t3 + " where "
+            for cn in self.range2:
+                try:
+                    thecell = row[cn]
+                except IndexError:
+                    thecell = ''
+                if not thecell == '':
+                    if self.lookup:
+                        beta = self.lookup2 + " = "
+                        beta = beta + "(select " + self.lookup + " from "
+                        beta = beta + self.t2 + " where " + self.c + " = '"
+                        beta = beta + (thecell).replace("'", r"\'")
+                        beta = beta + "' limit 1) or"
+                    else:
+                        beta = "'" + (thecell).replace("'", r"\'") + "'"
+                alpha = alpha + beta + " "
+            query = query + alpha[:-4] + "; \n "
+        return query + ";"  # remove last union and return
 
 if __name__ == "__main__":
     import sys
