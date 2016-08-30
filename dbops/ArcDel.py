533d54d847388e339eef21520d1781c92f2fda83
Ryan Birmingham <birm@rbirm.us>
2016-08-30 09:59:28 -0400

    start arcdel

---
+++ dbops/ArcDel.py
@@ -0,0 +1,6 @@
+class ArcDel(Object)
+    def __init__(self):
+        raise NotImplementedError("HealthCheck under construction")
+
+if __name__ == "__main__":
+    raise NotImplementedError("HealthCheck under construction")
