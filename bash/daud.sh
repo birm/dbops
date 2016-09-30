a7cdb2094ea6aa945f88841b3bec3d940961895e
Ryan Birmingham <birm@rbirm.us>
2016-09-30 11:26:48 -0400

    improve daud.sh

--- bash/daud.sh
+++ bash/daud.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Usage: <name> hosts like
+# Usage: ./daud hosts like
 like_host=$2
 for host in $(cat $1); do
+  echo "~~~ ### " $host " ### ~~~"
   echo "1 #### Configuration Differences #### 1"
   pt-config-diff h=$like_host h=$host
   echo "2 #### Variable Advising #### 2"
-  pt-variable advisor $host
+  pt-variable-advisor $host
   echo "3 #### Duplicate Keys #### 3"
   pt-duplicate-key-checker --host $host
 done
