9f16f7b1465f625088be5e78ad644f278455f583
Ryan Birmingham <birm@rbirm.us>
2016-09-30 11:23:13 -0400

    add db audit script

---
+++ bash/daud.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Usage: <name> hosts like
+like_host=$2
+for host in $(cat $1); do
+  echo "1 #### Configuration Differences #### 1"
+  pt-config-diff h=$like_host h=$host
+  echo "2 #### Variable Advising #### 2"
+  pt-variable advisor $host
+  echo "3 #### Duplicate Keys #### 3"
+  pt-duplicate-key-checker --host $host
+done
