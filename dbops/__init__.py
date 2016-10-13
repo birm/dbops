338978f2f8c1eda2db06a72e7337a2a97ed34e79
Birm <birm@rbirm.us>
2016-10-13 14:50:17 -0400

    remove projects

--- dbops/__init__.py
+++ dbops/__init__.py
@@ -1,13 +1,6 @@
 __all__ = ["WhereLives",
-           "DiskCheck",
-           "SqlParser",
-           "RhelKick",
            "PatchExpander"
-           "Xls2Sql"]
+           "HealthCheck"]
 from WhereLives import WhereLives
-from DiskCheck import DiskCheck
-from SqlParser import SqlParser
-from RhelKick import RhelKick
-from SchemaConv import SchemaConv
-from xls2sql import Xls2Sql
+from HealthCheck import HealthCheck
 from PatchExpander import PatchExpander
