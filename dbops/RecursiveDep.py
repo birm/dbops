afe8304bf1f9dea9ab08c75c770e1f0da79eaa1a
Ryan Birmingham <birm@rbirm.us>
2016-11-10 15:05:34 -0500

    added 'functions'

--- dbops/RecursiveDep.py
+++ dbops/RecursiveDep.py
@@ -1,15 +1,40 @@
 class RecursiveDep(object):
-    """Determine out what depends on a schema, and so on for schemas it depends on.
+
+    """
+    Map all dependencies for a database/schema, to advise changes.
         args:
             host: the hostname to use to connect to
             database: the database to check against
-            table: the table to scan for dependencies
-            form: the form to return the result"""
+            form: the form to return the result
+            """
 
     def __init__(self, host="localhost", database="mysql",
                  table="user", form="tree"):
         """create assertion object."""
         self.host = host
         self.database = database
-        self.table = table
         self.form = form
+        self.storage = set()  # set of lists for result
+
+    def find(self):
+        """Find, store, and show all dependencies."""
+        # get tables in db
+        # call _find_deps for all
+        pass
+        # call the appropriate result function
+
+    def _store(self, from_db, to_db):
+        """Store the result to internal variable."""
+        pass
+
+    def _find_deps(self, tablename):
+        """Find dependencies for a given table, given by name."""
+        pass
+
+    def _graph_result(self):
+        """The result display function for the graph output."""
+        pass
+
+    def _text_result(self):
+        """The result displa function for text or command line output."""
+        pass
