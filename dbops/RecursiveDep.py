4d9347af42a47bb96687ee0d153274211d36c1c8
Ryan Birmingham <birm@rbirm.us>
2016-11-11 11:26:58 -0500

    added connect deps to recursivedep

--- dbops/RecursiveDep.py
+++ dbops/RecursiveDep.py
@@ -15,6 +15,7 @@ class RecursiveDep(object):
         """create assertion object."""
         self.host = host
         self.database = database
+        self.table = table
         self.form = form
         self.storage = set()  # set of lists for result
 
@@ -46,6 +47,37 @@ class RecursiveDep(object):
         and referenced_column_name is not NULL;""".format(db=self.database,
                                                           table=tablename)
         return self._run_mysql(dep_query)
+
+    def _connect_deps(self, tablename, maxdep=5):
+        """Get the tree of dependencies for a table, up to maxdep times.
+            input:
+                tablename(str) - which table to start with.
+                maxdep(int) - (optional) how many layers deep to go
+            output:
+                """
+        connecting = True  # while condition set
+        # for each iteration
+        working = []  # list of tables to work through
+        working2 = [] # list of tables to add next iteration
+        result = []
+        pos = 0  # position for result, and for maxdep
+        working.append(tablename)
+        while connecting:
+            for table in working:
+                # remove from working
+                working.remove(table)
+                # all tables with relevant depenency
+                midres = [x[1] for x in self.storage if x[0]==table]
+                #add to working
+                working2.extend(midres)
+            # prepare for next level deep
+            working = list(set(working2))
+            working2 = []
+            # check if we should continue
+            result[pos] = working  # save the result
+            pos = pos + 1
+            if (not midres) or (pos >= maxdep):
+                    continue = False  # end the loop
 
     def _graph_result(self):
         """The result display function for the graph output."""
