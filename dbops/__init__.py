34426e066f41a55b5003385c58b3fd279a4235f8
Ryan Birmingham <birm@rbirm.us>
2016-12-01 12:49:45 -0500

    remove assertdb for new repo

--- dbops/__init__.py
+++ dbops/__init__.py
@@ -2,11 +2,9 @@ __all__ = ["WhereLives",
            "PatchExpander"
            "HealthCheck",
            "LogAuditor",
-           "AssertDB",
            "CommandMultiRun"]
 from dbops.WhereLives import WhereLives
 from dbops.HealthCheck import HealthCheck
 from dbops.PatchExpander import PatchExpander
 from dbops.LogAuditor import LogAuditor
-from dbops.AssertDB import AssertDB
 from dbops.CommandMultiRun import CommandMultiRun
