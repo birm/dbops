85a2db81804d836ea3a5bf350201ebab0386a5aa
Ryan Birmingham <birm@rbirm.us>
2016-12-01 12:51:20 -0500

    remove assertdb from test

--- dbops/tests/test_default_inits.py
+++ dbops/tests/test_default_inits.py
@@ -1,16 +1,12 @@
 from dbops import WhereLives
 from dbops import PatchExpander
 from dbops import CommandMultiRun
-from dbops import AssertDB
 
 def test_wherelives_default_init():
     wl = WhereLives()
 
 def test_patchexpander_default_init():
     pe = PatchExpander()
-
-def test_assertdb_default_init():
-    ad = AssertDB()
 
 def test_commandmultirun_default_init():
     text_file = open("food.pmr", "w")
